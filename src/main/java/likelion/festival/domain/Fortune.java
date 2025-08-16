31b3332bfb22fd0bf774895ca57817e64a690249
kyoohyunlee <khyhlee@naver.com>
2025-08-17 00:02:30 +0900

    feat: 운세, 유저별운세 엔티티 생성

---
+++ src/main/java/likelion/festival/domain/Fortune.java
@@ -0,0 +1,62 @@
+package likelion.festival.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Fortune {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String message;
+
+    @Column(nullable = false)
+    private int score;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
