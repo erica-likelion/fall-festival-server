64ba715b2a2570688509a73b60aa8860d4782b8d
kyoohyunlee <khyhlee@naver.com>
2025-08-20 02:48:11 +0900

    feat: 중복된 운세 매핑 안되게

--- src/main/java/likelion/festival/repository/FortuneRepository.java
+++ src/main/java/likelion/festival/repository/FortuneRepository.java
@@ -4,5 +4,4 @@ import likelion.festival.domain.Fortune;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FortuneRepository extends JpaRepository<Fortune, Long> {
-    Fortune findFortuneById(Long randomFortuneId);
 }
