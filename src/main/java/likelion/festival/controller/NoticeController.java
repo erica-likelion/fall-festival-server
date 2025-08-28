ce432a55cb096547f3b5c52185a419b3faf0d11e
임진수 <jsbd0109@naver.com>
2025-08-29 02:33:50 +0900

    feat: 공지사항 getById 컨트롤러 생성

--- src/main/java/likelion/festival/controller/NoticeController.java
+++ src/main/java/likelion/festival/controller/NoticeController.java
@@ -19,4 +19,11 @@ public class NoticeController {
     public ApiSuccess<List<NoticeDto>> getNotices() {
         return new ApiSuccess<>(noticeService.getAll(), "공연 목록 조회 완료");
     }
+
+    @GetMapping("/{id}")
+    public ApiSuccess<NoticeDto> getNotice(@PathVariable Long id) {
+        NoticeDto noticeDto = noticeService.getById(id);
+
+        return new ApiSuccess<>(noticeDto, "공지 상세 조회 성공");
+    }
 }
