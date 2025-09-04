8fd41ccf4989e364bc848748f59d5205cef426c2
임진수 <jsbd0109@naver.com>
2025-09-04 23:10:51 +0900

    fix: Notice 엔티티 이미지 url 리스트로 수정

--- src/main/java/likelion/festival/domain/Notice.java
+++ src/main/java/likelion/festival/domain/Notice.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table(name = "notices")
 @Getter
@@ -19,8 +21,10 @@ public class Notice {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
-    @Column(length = 500) // 공지에 사진이 없는 경우가 있을까봐 혹시 몰라서 일단 nullable = true로 했습니다.
-    private String image;
+    @ElementCollection
+    @CollectionTable(name = "notice_images" , joinColumns = @JoinColumn(name = "notice_id")) // 테이블 안에 리스트 컬럽을 직접 저장 불가해서 이런 방식 사용
+    @Column(name = "image_url", nullable = false, length = 500) // 어차피 인스타 글 따올거니까 무조건 사진 있을 예정. nullable = false로 수정
+    private List<String> images;
 
     @Column(nullable = false, length = 50)
     private String tag;
