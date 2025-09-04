d3189dd9628e698095b5785130cb2b6634554883
이규현 <khyhlee@naver.com>
2025-09-04 23:08:52 +0900

    Update src/main/java/likelion/festival/repository/ContentRepository.java
    Co-authored-by: Seochan Moon <moonsc1110@gmail.com>

--- src/main/java/likelion/festival/repository/ContentRepository.java
+++ src/main/java/likelion/festival/repository/ContentRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
-    @Query("SELECT c  FROM Content c WHERE :now BETWEEN c.startTime AND c.endTime ORDER BY c.startTime DESC, c.endTime ASC")
+    @Query("SELECT c FROM Content c WHERE :now BETWEEN c.startTime AND c.endTime ORDER BY c.startTime DESC, c.endTime ASC")
     List<Content> findActiveContents(@Param("now") LocalDateTime now);
 
 }
