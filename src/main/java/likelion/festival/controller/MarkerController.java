573530245fbdac1ed65a586c44edd8fd48851ed3
Seochan Moon <moonsc1110@gmail.com>
2025-09-16 19:08:00 +0900

    feat: CacheControl 헤더 추가

--- src/main/java/likelion/festival/controller/MarkerController.java
+++ src/main/java/likelion/festival/controller/MarkerController.java
@@ -4,12 +4,14 @@ import likelion.festival.dto.MarkerResponseDto;
 import likelion.festival.exception.ApiSuccess;
 import likelion.festival.service.MarkerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 마커 관련 REST API 컨트롤러
@@ -27,8 +29,10 @@ public class MarkerController {
     @GetMapping
     public ResponseEntity<ApiSuccess<List<MarkerResponseDto>>> getAllMarkers() {
         List<MarkerResponseDto> markers = markerService.getAllMarkers();
-        return ResponseEntity.ok(ApiSuccess.of(markers, "마커 목록 조회 완료"));
-    }
 
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(30, TimeUnit.MINUTES))
+                .body(ApiSuccess.of(markers, "마커 목록 조회 완료"));
+    }
 }
 
